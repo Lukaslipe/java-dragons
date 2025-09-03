8e19c0f142cff735b6175ab73b8e02b872233a6e
lukaslipe <lucascristodev@gmail.com>
2025-09-03 20:36:50 -0300

    Feat: Criação da classe heroi e inimigo

---
+++ src/Inimigo.java
@@ -0,0 +1,25 @@
+public class Inimigo extends Personagem{
+
+    protected int dano;
+    protected String tipo;
+
+    public Inimigo(String nome, int vida, int nivel, int dano, String tipo) {
+        super(nome, vida, nivel);
+        this.dano = dano;
+        this.tipo = tipo;
+    }
+
+    @Override
+    public boolean estaVivo() {
+        return vida > 0;
+    }
+
+    public void atacar(Heroi heroi) {
+        heroi.receberDano(dano);
+    }
+
+    public void receberDano(int dano) {
+        vida -= dano;
+        if (vida < 0) vida = 0;
+    }
+}
