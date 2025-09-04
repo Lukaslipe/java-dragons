27ad82dfa4bbb37fc0ce325cabddf9b676e8cd32
elo-zlotek <zelolo2006@gmail.com>
2025-09-03 21:10:57 -0300

    Feat: classe Curandeiro

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,25 @@ public class Main {
 
         Heroi lucas = new Heroi("Lucas", 100, 1, 10, 20, "Espada");
 
-        System.out.println(lucas.getNome());
+        System.out.println(lucas.getVida());
+
+        Inimigo nino = new Inimigo("Nino", 200, 1, 30, "lobo");
+
+        nino.atacar(lucas);
+
+        System.out.println(lucas.getVida());
+
+        System.out.println(nino.getVida());
+
+        lucas.atacar(nino);
+
+        System.out.println(nino.getVida());
+
+        Curandeiro deus = new Curandeiro("GOD",1000,2000,10);
+
+        deus.curar(lucas);
+
+        System.out.println(lucas.getVida());
+
     }
 }
