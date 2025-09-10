f1ee2325b5d8e0b1889b7ecf4def36e2a2c063af
lukaslipe <lucascristodev@gmail.com>
2025-09-10 19:47:53 -0300

    Feat: Alterações na main para apresentação

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-
+        Heroi heroi = null;
+        Historia historia = new Historia();
         try {
-            Heroi heroi = new Heroi("Lucas", 100, 1, 10, 0, "Espada");
+            heroi = new Heroi("Lucas", 100, 1, 10, 0, "Espada");
         } catch (IllegalArgumentException e) {
             System.out.println("Erro ao criar herói: " + e.getMessage());
         }
 
+        try {
+            heroi = new Heroi("Lucas", 100, 1, 10, 10, "Espada");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Erro ao criar herói: " + e.getMessage());
+        }
 
+        Inimigo inimigo = new Inimigo("Dragão", 100, 1, 10,  "Dragão");
+
+        if (heroi != null) {
+            historia.iniciar(heroi);
+        } else {
+            System.out.println("Não foi possível iniciar a batalha, herói inválido!");
+        }
     }
 }
